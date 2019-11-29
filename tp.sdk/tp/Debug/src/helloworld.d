c90013d41eef55d072e9b6a3e7cb33b3d848d2aa
Pablo Slavkin <pslavkin@disenioconingenio.com.ar>
2019-11-29 18:06:29 -0300

    despues de mucho probar arranco con un blinky limpio
    bueno, mas o menos le agarre la onda. Hice un nuevo proyecto desde cero
    con el zq y un blinky, la idea es usar la uart para mandar los datos al
    PL

---
+++ tp.sdk/tp/Debug/src/helloworld.d
@@ -0,0 +1,51 @@
+src/helloworld.o src/helloworld.o: ../src/helloworld.c \
+ ../../tp_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../tp_bsp/ps7_cortexa9_0/include/xparameters_ps.h ../src/platform.h \
+ ../src/platform_config.h \
+ ../../tp_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../tp_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../tp_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../tp_bsp/ps7_cortexa9_0/include/bspconfig.h \
+ ../../tp_bsp/ps7_cortexa9_0/include/xgpio.h \
+ ../../tp_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../tp_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../tp_bsp/ps7_cortexa9_0/include/xgpio_l.h \
+ ../../tp_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../tp_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../tp_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../tp_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../tp_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h
+
+../../tp_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../tp_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+../../tp_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../tp_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../tp_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../tp_bsp/ps7_cortexa9_0/include/bspconfig.h:
+
+../../tp_bsp/ps7_cortexa9_0/include/xgpio.h:
+
+../../tp_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../tp_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../tp_bsp/ps7_cortexa9_0/include/xgpio_l.h:
+
+../../tp_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../tp_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../tp_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../tp_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../tp_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
---
+++ tp.sdk/tp/Debug/src/platform.d
@@ -0,0 +1,15 @@
+src/platform.o src/platform.o: ../src/platform.c \
+ ../../tp_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../tp_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../tp_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../tp_bsp/ps7_cortexa9_0/include/xil_types.h ../src/platform_config.h
+
+../../tp_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../tp_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../tp_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../tp_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../src/platform_config.h:
