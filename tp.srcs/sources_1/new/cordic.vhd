fee9f3dd037d32da8a5ce15cf5fa42940cb75240
Pablo Slavkin <pslavkin@disenioconingenio.com.ar>
2020-01-09 20:26:16 -0300

    cordic con frenillos!
    list, le hice 2 procesos, uno comunica para afuera axi y para adentro el
    primer instancia de cordic del pipeline, y el posCordic atiende al
    ultimo pipe y a la salida con otro axi stream. de modo que desde afuera
    no pasa nada es un modulo mas con interfaz axi, y para adentro, el
    pipeline se frena y se auto gestiona cuando no puede despachar cosas.
    Lo malo es que tengo 2 clk que pierdo en esa gestion. Por un lado pierdo
    2 clks porque los datos vienen de 8 bits porque quiero mantener
    compatibilidad, pero por otro lado pierdo 2 mas haciendo el handshaking
    de pasar de recibir datos desde el axi a mandarselo al cordic.
    La axi de salida no agrega delay porque solo tiene que mandar 1 byte..
    con lo cual no pasa nada.. la que frena es la de la entrada, pero se
    puede ver como se va llenando el pipe y todo.. muy copado.. a dormir..

--- tp.srcs/sources_1/new/cordic.vhd
+++ tp.srcs/sources_1/new/cordic.vhd
@@ -48,45 +48,39 @@ architecture Behavioral of cordic is
    end component; --}}}
 
    constant MAX_ITER : natural := 10;  -- maximo largo de la tabla. ITER puede ir de aca para abajo 
-   type   axiStates   is (waitingSvalid, waitingMready,waitingCordic);
-   signal state     : axiStates := waitingSvalid;
+   type   axiStates   is (waitingSvalid, waitingMready);
+   signal preState  : axiStates := waitingSvalid;
+   signal posState  : axiStates := waitingSvalid;
    type   xyDataArray is array (0 to 1) of std_logic_vector ( 7 downto 0 );
    signal xyData    : xyDataArray;
-   signal clockWise : std_logic := '0';
    signal angle     : std_logic_vector(N-1 downto 0):= (others=>'0');
 
 
    type   handShakeVector is array ( ITER downto 0 )of std_logic                     ;
    type   ConnectVector is array   ( ITER downto 0 )of std_logic_vector(N-1 downto 0);
    type   intLUT is array          ( MAX_ITER-1 downto 0 )of integer range 0 to 5000  ; --la tabla soporta hasta MAX_ITER, pero en la instanciacino del cordic se puede elejir menos o igual.. no mas porque reviente.. la tabla no tendria datos..
-   signal validW,  readyW,inv  : handShakeVector:= (others=>'0');
+   signal validW, readyW, inv  : handShakeVector:= (others=>'0');
    signal wirex, wirey, wirez, wireLUT: ConnectVector;
    signal atanLUT                     : intLUT       := (4500, 2657, 1404, 713, 358, 179, 90, 45, 22, 11);
 
 begin
-   cordic_proc:process (clk) is --{{{
+   pre_cordic_proc:process (clk) is --{{{
       variable bitCounter :integer range 0 to 8 ;
-
-      variable sign :signed (15 downto 0);
-      variable extension :std_logic_vector (15 downto 0);
    begin
       if rising_edge(clk) then
          if rst = '0' then
-            state         <= waitingSvalid;
+            preState      <= waitingSvalid;
             s_axis_tready <= '1';
-            m_axis_tvalid <= '0';
-            m_axis_tdata  <= (others => '0');
-            clockWise     <= '0';
-            angle         <= (others => '0');
-            validW(0)     <= '0';                           --y ya no tengo mas nada
-            inv(0)        <= '0';                           --y ya no tengo mas nada
+            validW(0)     <= '0';
+            inv(0)        <= '0';
             bitCounter    := 0;
          else
-            case state is
+            case preState is
                when waitingSvalid =>
                   if s_axis_tvalid = '1' then                           --espero e que este listo para enviar algo
+                     bitCounter := bitCounter + 1;
                      case bitCounter is
-                        when 0 =>
+                        when 1 =>
                           wirex(0) <= (others=>'0');
                           if s_axis_tdata(7)='1' then
                              inv(0)   <= '1';
@@ -95,7 +89,7 @@ begin
                              inv(0)   <= '0';
                              wirex(0) <= std_logic_vector(to_signed(to_integer(signed(s_axis_tdata)),16));
                           end if;
-                        when 1 =>
+                        when 2 =>
                           wirey(0) <= (others=>'0');
                           wirey(0) <= std_logic_vector(to_signed(to_integer(signed(s_axis_tdata)),16));
                           if inv(0)='1' then
@@ -105,52 +99,65 @@ begin
                                 wirez(0) <= std_logic_vector(to_signed(-18000,wirez(0)'length));
                              end if;
                           else 
-                                wirez(0) <= (others=>'0');
+                             wirez(0) <= (others=>'0');
                           end if;
                           validW(0)     <= '1';
                           s_axis_tready <= '0';                           --entonces yo tambien estoy listo
-                          m_axis_tvalid <= '0';                           --y ya no tengo mas nada
-                          state         <= waitingCordic;
+                          preState      <= waitingMready;
                         when others =>
                      end case;
-                     bitCounter := bitCounter + 1;
                   end if;
-               when waitingCordic =>
-                  validW(0)     <= '0';
-                  readyW(ITER)     <= '1';
-                  if validW(ITER) = '1' then                           --espero e que este listo para enviar algo
-                     readyW(ITER)  <= '0';
-                     m_axis_tdata  <= wirex(ITER-1)(7 downto 0);
-                     m_axis_tvalid <= '1';                           --y ya no tengo mas nada
-                     state         <= waitingMready;
+               when waitingMready =>
+                  if readyW(0) = '1' then                           --espero e que este listo para enviar algo
+                     validW(0)     <= '0';
+                     s_axis_tready <= '1';                           --entonces yo tambien estoy listo
+                     preState      <= waitingSvalid;
                      bitCounter    := 0;
                   end if;
-
-               when waitingMready =>
-                  if m_axis_tready = '1' then                      --lo puedo empezar a mandar al otro lado?
-                    bitCounter := bitCounter+1;                   --incremento
-                     case bitCounter is
-                        when 1 =>
-                           m_axis_tdata <= wirey(ITER-1)(7 downto 0);
-                        when 2 =>
-                           if(inv(ITER-1)='1') then
-                              angle <= std_logic_vector(-signed(wirez(ITER-1)));
-                              m_axis_tdata <= std_logic_vector(-signed(wirez(ITER-1)(14 downto 7)));
-                           else
-                              angle <= std_logic_vector(signed(wirez(ITER-1)));
-                              m_axis_tdata <= wirez(ITER-1)(14 downto 7);
-                           end if;
-                           m_axis_tvalid <= '0';                      --y aviso que no tengo mas nada que mandar state         <= waitingSvalid;                 --termine de mandar, vuelvo a esperar
-                           s_axis_tready <= '1';
-                           bitCounter    := 0;
-                           state         <= waitingSvalid;            --cambio de estado, y le doy un clk para que ponga el dato
-                        when others =>
-                     end case;
-                  end if;
+               when others =>
             end case;
          end if;
       end if;
-   end process cordic_proc; --}}}
+   end process pre_cordic_proc; --}}}
+
+   pos_cordic_proc:process (clk) is --{{{
+      variable bitCounter :integer range 0 to 8 ;
+   begin
+      if rising_edge(clk) then
+         if rst = '0' then
+            posState      <= waitingSvalid;
+            m_axis_tvalid <= '0';
+            m_axis_tdata  <= (others => '0');
+            angle         <= (others => '0');
+            readyW(ITER)  <= '1';                           --y ya no tengo mas nada
+            bitCounter := 0;
+         else
+            case posState is
+               when waitingSvalid =>
+                  if validW(ITER) = '1' then                           --espero e que este listo para enviar algo
+                     if(inv(ITER-1)='1') then
+                        angle <= std_logic_vector(-signed(wirez(ITER-1)));
+                        m_axis_tdata <= std_logic_vector(-signed(wirez(ITER-1)(14 downto 7)));
+                     else
+                        angle <= std_logic_vector(signed(wirez(ITER-1)));
+                        m_axis_tdata <= wirez(ITER-1)(14 downto 7);
+                     end if;
+                     readyW(ITER)  <= '0';
+                     m_axis_tvalid <= '1';
+                     bitCounter    := 0;
+                     posState      <= waitingMready;
+                  end if;
+                  when waitingMready =>
+                     if m_axis_tready = '1' then
+                        bitCounter    := 0;
+                        posState      <= waitingSvalid;            --cambio de estado, y le doy un clk para que ponga el dato
+                        m_axis_tvalid <= '0';                      --y aviso que no tengo mas nada que mandar state         <= waitingSvalid;                 --termine de mandar, vuelvo a esperar
+                        readyW(ITER)  <= '1';
+                     end if;
+               end case;
+            end if;
+         end if;
+   end process pos_cordic_proc; --}}}
 
    connection_instance: for j in 0 to ITER-1 generate
    begin
